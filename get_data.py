d9692327aafa5f4a140456bc900aeea893dd61fa
Mosha Langerak <82883699+MoshaLangerak@users.noreply.github.com>
2024-10-01 09:29:25 +0200

    Update for getting data

--- get_data.py
+++ get_data.py
@@ -1,24 +1,30 @@
 from datetime import datetime
 import pandas as pd
-from pandas import DataFrame
 import yfinance as yf
 import time
-import bs4 as bs
-import pickle
-import requests
+import os
 
-def download_stock(stock):
+def get_price_data(stock):
 	""" try to query the iex for a stock, if failed note with print """
-	try:
-		ticker = yf.Ticker(stock)
-		stock_df = ticker.history(period="5y")
-		output_name = 'data/' + stock + '_data.csv'
-		stock_df.to_csv(output_name)
+	ticker = yf.Ticker(stock)
+	stock_df = ticker.history(period="5y")
+	output_name = 'data/' + stock + '_data.csv'
+	stock_df.to_csv(output_name)
 
-	except Exception as e:
-		print(e)
-		bad_names.append(stock)
-		print('bad: %s' % (stock))
+def get_info_data(stock, info_to_keep):
+	""" try to query the iex for a stock, if failed note with print """
+	ticker = yf.Ticker(stock)
+	stock_info = ticker.info
+	stock_info = {}
+
+	for key in info_to_keep:
+		try:
+			stock_info[key] = stock_info[key]
+		except:
+			stock_info[key] = None
+
+	stock_info['ticker'] = stock
+	return stock_info
 
 def read_s_and_p(file):
 	""" read in s&p data """
@@ -28,22 +34,30 @@ def read_s_and_p(file):
 if __name__ == '__main__':
 	""" list of S&P companies """
 	s_and_p = read_s_and_p('sp500.csv')
-		
+	
+	info_to_keep = ['country', 'industry', 'sector', 'fullTimeEmployees', 'auditRisk', 
+				 'boardRisk', 'compensationRisk', 'overallRisk', 'dividendRate', 'dividendYield', 
+				 'marketCap', 'profitMargins', 'totalCash', 'totalCashPerShare', 'ebitda', 
+				 'totalDebt', 'totalRevenue', 'revenuePerShare', 'grossMargins']
+	
+	info_df = pd.DataFrame(columns=['ticker'] + info_to_keep)
+	
 	start_time = datetime.now()
-	bad_names =[] #to keep track of failed queries
 
 	for stock in s_and_p:
-		download_stock(stock)
+		if os.path.exists('data/' + stock + '_data.csv'):
+			continue
+
+		get_price_data(stock)
+		info = get_info_data(stock, info_to_keep)
+		info_df = pd.concat([info_df, pd.DataFrame(info, index=[0])], axis=0)
+
 		time.sleep(.5) #so I don't get booted from the server for an abuse of the API
-	
-	# Save failed queries to a text file to retry
-	if len(bad_names) > 0:
-		with open('failed_queries.txt','w') as outfile:
-			for name in bad_names:
-				outfile.write(name+'\n')
+
+	info_df.to_csv('data/info.csv', index=False)
 
 	#timing:
 	finish_time = datetime.now()
 	duration = finish_time - start_time
 	minutes, seconds = divmod(duration.seconds, 60)
-	print(f'The threaded script took {minutes} minutes and {seconds} seconds to run.')
+	print(f'The script took {minutes} minutes and {seconds} seconds to run.')
