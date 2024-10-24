03e7259dbba2bb77f43e9124b40a8929854dacb2
Mosha Langerak <82883699+MoshaLangerak@users.noreply.github.com>
2024-10-24 17:24:30 +0200

    Move get_data and update load_data

--- load_data.py
+++ load_data.py
@@ -3,11 +3,30 @@ import pickle
 import numpy as np
 
 def load_data_from_pickle(file_path):
+    """
+    Load data from a pickle file.
+
+    Parameters:
+        file_path (str): The file path to the pickle file.
+
+    Returns:
+        pd.DataFrame: The data loaded from the pickle file.
+    """
     with open(file_path, 'rb') as f:
         data = pickle.load(f)
 
     return pd.DataFrame(data)
 
 def load_data_from_csv(file_path):
+    """
+    Load data from a CSV file.
+
+    Parameters:
+        file_path (str): The file path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The data loaded from the CSV file.
+    """
+    
     return pd.read_csv(file_path, index_col=0)
 
